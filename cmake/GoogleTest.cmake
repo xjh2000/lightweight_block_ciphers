9ac67eb7c7d1b14894e22aec2782a9f1b0027110
xjh <934622645@qq.com>
2023-02-01 13:22:01 +0800

    for all platform cmake build

--- cmake/GoogleTest.cmake
+++ cmake/GoogleTest.cmake
@@ -1,3 +1,9 @@
-# find by linux
-find_package(GTest REQUIRED)
-include_directories(${GTEST_INCLUDE_DIRS})
+# for all platform
+include(FetchContent)
+FetchContent_Declare(
+        googletest
+        URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
